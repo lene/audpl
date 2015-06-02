13aa7a963a30cada24ec0f9430b8ce3845f4fb57
Lene Preuss <lene.preuss@gmail.com>
2015-06-02 15:58:58 +0200

    remove bash files in home dir when installing

--- install.sh
+++ install.sh
@@ -2,4 +2,5 @@
 
 ln -s $HOME/workspace/config/audacious $HOME/.config
 ln -s $HOME/workspace/config/openbox $HOME/.config
-ln -s $HOME/workspace/config/bash/.??* $HOME
+#ln -s $HOME/workspace/config/bash/.??* $HOME
+for i in bash/.??*; do file=$(basename $i);  rm -r $HOME/$file && ln -s $(pwd)/$i $HOME/$file; done
