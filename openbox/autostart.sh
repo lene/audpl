41cada3577de59eb104e9a47062f1195cc708660
Lene Preuss <lene.preuss@gmail.com>
2016-05-14 21:10:08 +0200

    Titan - Sa 14. Mai 21:10:07 CEST 2016

--- openbox/autostart.sh
+++ openbox/autostart.sh
@@ -7,5 +7,5 @@ nitrogen --restore &
 sleep 5 && kdeinit4 &
 
 test -x /opt/toggldesktop/TogglDesktop.sh && /opt/toggldesktop/TogglDesktop.sh &
-which btsync && sleep 60 && btsync --storage ~/.btsync
-which slack && sleep 120 && slack &
+#which btsync && sleep 60 && btsync --storage ~/.btsync
+#which slack && sleep 120 && slack &
