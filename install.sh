9669b94bb6e40a10c0e7c435d602c722d5624ee9
Lene Preuss <lene.preuss@gmail.com>
2016-07-15 16:25:24 +0200

    added compilation and execution of script which changes desktop layout to 3x3 instead of 9x1

--- install.sh
+++ install.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-ln -s $HOME/workspace/config/audacious $HOME/.config
-ln -s $HOME/workspace/config/openbox $HOME/.config
+ln -s $HOME/workspace/configs/audacious $HOME/.config
+ln -s $HOME/workspace/configs/openbox $HOME/.config
 #ln -s $HOME/workspace/config/bash/.??* $HOME
-for i in bash/.??*; do file=$(basename $i);  rm -r $HOME/$file && ln -s $(pwd)/$i $HOME/$file; done
+for i in bash/.??*; do file=$(basename $i);  rm -rf $HOME/$file && ln -s $(pwd)/$i $HOME/$file; done
+
+if [ -f openbox/setlayout.c ]; then
+    cd openbox && \
+	sudo apt install libx11-dev && \
+	gcc setlayout.c -o setlayout -lX11 && \
+	./setlayout 0 3 3 0 && \
+	cd ..
+fi
