95b51a1d230009520aa0e4f5059ebe4873e6e134
Lene Preuss <lene.preuss@gmail.com>
2016-07-21 09:40:52 +0200

    fixed merge conflict

--- openbox/autostart.sh
+++ openbox/autostart.sh
@@ -2,6 +2,15 @@
 
 setxkbmap -layout us
 
+if [ -x ~/workspace/configs/openbox/setlayout ]; then
+    ~/workspace/configs/openbox/setlayout 0 3 3 0
+else
+    if [ - f ~/workspace/configs/openbox/setlayout.c ]; then
+	gcc ~/workspace/configs/openbox/setlayout.c -o ~/workspace/configs/openbox/setlayout
+	~/workspace/configs/openbox/setlayout 0 3 3 0
+    fi
+fi
+    
 gkrellm &
 xfce4-panel &
 nitrogen --restore &
