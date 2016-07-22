6935323a71a6477449e25cba7b5f9470e9ffe3cb
Lene Preuss <lene.preuss@gmail.com>
2016-07-22 17:18:20 +0200

    fixed typo

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 ln -s $HOME/workspace/configs/audacious $HOME/.config
 ln -s $HOME/workspace/configs/openbox $HOME/.config
-#ln -s $HOME/workspace/config/bash/.??* $HOME
+#ln -s $HOME/workspace/configs/bash/.??* $HOME
 for i in bash/.??*; do file=$(basename $i);  rm -rf $HOME/$file && ln -s $(pwd)/$i $HOME/$file; done
 
 if [ -f openbox/setlayout.c ]; then
