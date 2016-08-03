85dd79864cd4a2b951392c841591d0b8e9a4d77b
Lene Preuss <lene.preuss@here.com>
2016-08-03 14:31:31 +0200

    added widow manager and kde global config

--- install.sh
+++ install.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
 ln -s $HOME/workspace/configs/audacious $HOME/.config
-ln -s $HOME/workspace/configs/openbox $HOME/.config
-#ln -s $HOME/workspace/configs/bash/.??* $HOME
-for i in bash/.??*; do file=$(basename $i);  rm -rf $HOME/$file && ln -s $(pwd)/$i $HOME/$file; done
+ln -s $HOME/workspace/configs/openbox   $HOME/.config
+
+for i in bash/.??*; do
+    file=$(basename $i)
+    rm -rf $HOME/$file && ln -s $(pwd)/$i $HOME/$file
+done
+
 ln -s $HOME/workspace/configs/kglobalshortcutsrc $HOME/.config
+ln -s $HOME/workspace/configs/kdeglobals         $HOME/.config
+ln -s $HOME/workspace/configs/kwinrc             $HOME/.config
+ln -s $HOME/workspace/configs/kwinrulesrc        $HOME/.config
 
 if [ -f openbox/setlayout.c ]; then
     cd openbox && \
