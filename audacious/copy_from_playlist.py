3f3d595f610497482ee49ab0b888273f0f38adf0
Lene Preuss <lene.preuss@here.com>
2016-09-20 16:43:04 +0200

    work laptop Di 20. Sep 16:43:04 CEST 2016

--- audacious/copy_from_playlist.py
+++ audacious/copy_from_playlist.py
@@ -1,34 +1,48 @@
 from urllib import unquote
-from os.path import isfile
+from os.path import isfile, join
+from shutil import copy2
 from argparse import ArgumentParser
 
-playlist = '1035'
+
+import os
+
+
+def find_first_dir(name, path):
+    for root, dirs, files in os.walk(path):
+        if name in dirs:
+            return os.path.join(root, name)
+
+
+def playlist_directory():
+    return find_first_dir('playlists', '.')
+    return 'playlists'
 
 
 def copy_playlist(playlist, number, target):
-    with open('playlists/'+playlist+'.audpl') as playlist_file:
+    with open(join(playlist_directory(), playlist+'.audpl')) as playlist_file:
         lines = playlist_file.readlines()
 
-    lines = [
+    files = [
         unquote(line[len('uri=file://'):]).strip()
         for line in lines if line.startswith('uri=file://')
         ]
-    print([line for line in lines if isfile(line)])
+    files = [line for line in files if isfile(line)]
+    for file in files[:number]:
+        copy2(file, target)
 
 
 def main(args):
-    parser = ArgumentParser(description="copy first N files of a playlist to target folder")
-    parser.add_argument(
-        '-p', '--playlist', default='', type=str,
-        help='Name of the playlist to copy'
+    parser = ArgumentParser(
+        description="Copy the first N files of a playlist to a target folder"
     )
     parser.add_argument(
-        '-n', '--number', default=0, type=int,
-        help='First N files to copy from the playlist'
+        '-p', '--playlist', required=True, type=str, help='Name of the playlist to copy'
     )
     parser.add_argument(
-        '-t', '--target', default='', type=str,
-        help='Name of the target directory'
+        '-n', '--number', default=-1, type=int, help='First N files to copy from the playlist'
+    )
+    parser.add_argument(
+        '-t', '--target', default='.', type=str, help='Name of the target directory'
     )
     opts = parser.parse_args(args)
     copy_playlist(opts.playlist, opts.number, opts.target)
