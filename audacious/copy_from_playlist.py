c643571ca6b5f8ea56e2db05aa5b2323f9cd0974
Lene Preuss <lene.preuss@here.com>
2016-10-07 14:43:05 +0200

    work laptop Fr 7. Okt 14:43:05 CEST 2016

--- audacious/copy_from_playlist.py
+++ audacious/copy_from_playlist.py
@@ -6,9 +6,65 @@ from shutil import copy2
 from argparse import ArgumentParser
 from subprocess import check_output
 
-PLAYLIST_DIR_NAME = 'playlists'
-PLAYLIST_EXTENSION = '.audpl'
-FILE_LINE_PREFIX = 'uri=file://'
+
+class AudaciousTools:
+    """Tools for working with the audacious media player"""
+
+    PLAYLIST_DIR_NAME = 'playlists'
+    PLAYLIST_EXTENSION = '.audpl'
+    FILE_LINE_PREFIX = 'uri=file://'
+
+    def __init__(self, base_config_dir):
+        """
+        :param base_config_dir: directory under which the audacious playlists are searched
+        """
+        self._base_directory = base_config_dir
+        self._playlist_dir = None
+
+    def get_currently_playing_playlist_filename(self):
+        order = self.get_playlist_order()
+        return order[AudaciousTools._currently_playing_playlist_number() - 1]
+
+    def get_playlist_order(self):
+        with open(self._playlist_order_file_path()) as order_file:
+            return order_file.readlines()[0].split(' ')
+
+    @property
+    def playlist_directory(self):
+        if self._playlist_dir is None:
+            self._playlist_dir = find_first_dir(self.PLAYLIST_DIR_NAME, self._base_directory)
+        return self._playlist_dir
+
+    def files_in_playlist(self, playlist=None):
+        lines = self._read_playlist(playlist)
+        return existing_files(AudaciousTools._file_entries(lines))
+
+    def _playlist_order_file_path(self):
+        return os.path.join(self.playlist_directory, 'order')
+
+    def _read_playlist(self, playlist):
+        with open(self._playlist_file_path(playlist)) as playlist_file:
+            return playlist_file.readlines()
+
+    def _playlist_file_path(self, playlist):
+        return os.path.join(
+            self.playlist_directory, playlist + AudaciousTools.PLAYLIST_EXTENSION
+        )
+
+    @staticmethod
+    def _file_entries(lines):
+        return [
+            unquote(line[len(AudaciousTools.FILE_LINE_PREFIX):]).strip()
+            for line in lines if line.startswith(AudaciousTools.FILE_LINE_PREFIX)
+        ]
+
+    @staticmethod
+    def _currently_playing_playlist_number():
+        return int(check_output(['audtool', 'current-playlist']))
+
+
+def existing_files(files):
+    return [file for file in files if os.path.isfile(file)]
 
 
 def find_first_dir(name, path):
@@ -17,34 +73,14 @@ def find_first_dir(name, path):
             return os.path.join(root, name)
 
 
-def playlist_directory():
-    return find_first_dir(PLAYLIST_DIR_NAME, '.')
-
-
-def get_currently_playing_playlist():
-    playlist_number = int(check_output(['audtool', 'current-playlist']))
-    with open(os.path.join(playlist_directory(), 'order')) as order_file:
-        order = order_file.readlines()[0].split(' ')
-    playlist = order[playlist_number - 1]
-    return playlist
-
-
-def files_in_playlist(playlist):
-    if not playlist:
-        playlist = get_currently_playing_playlist()
-    with open(os.path.join(playlist_directory(), playlist + PLAYLIST_EXTENSION)) as playlist_file:
-        lines = playlist_file.readlines()
-    files = [
-        unquote(line[len(FILE_LINE_PREFIX):]).strip()
-        for line in lines if line.startswith(FILE_LINE_PREFIX)
-        ]
-    return [line for line in files if os.path.isfile(line)]
-
-
 def copy_playlist(playlist, number, target):
     if not os.path.isdir(target):
         os.mkdir(target)
-    for file in files_in_playlist(playlist)[:number]:
+
+    audacious = AudaciousTools('.')
+    if not playlist:
+        playlist = audacious.get_currently_playing_playlist_filename()
+    for file in audacious.files_in_playlist(playlist)[:number]:
         copy2(file, target)
 
 
