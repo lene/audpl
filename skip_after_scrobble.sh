d29a565758603115c1a164ef582a1889b2a1d3e1
Lene Preuss <lene.preuss@here.com>
2016-10-13 09:43:04 +0200

    work laptop Do 13. Okt 09:43:04 CEST 2016

--- skip_after_scrobble.sh
+++ skip_after_scrobble.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 inotifywait -qq -e close_write $HOME/.config/audacious/scrobbler.log && \
-	audtool current-song && \
-	audtool playlist-delete $(audtool playlist-position)
+  audtool current-song && \
+  audtool playlist-delete $(audtool playlist-position)
