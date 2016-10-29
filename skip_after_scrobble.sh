6692f4cdc4a8a03f739d7bcdfc6a605a64d11194
Lene Preuss <lene.preuss@gmail.com>
2016-10-29 15:48:32 +0200

    added short sleep so script can be called directly after the last one finished

--- skip_after_scrobble.sh
+++ skip_after_scrobble.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 inotifywait -qq -e close_write $HOME/.config/audacious/scrobbler.log && \
   audtool current-song && \
-  audtool playlist-delete $(audtool playlist-position)
+  audtool playlist-delete $(audtool playlist-position) && \
+  sleep 2
+
+
