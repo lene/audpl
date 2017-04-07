b132bd43236802bc93cf70393b45eec7de761c83
Lene Preuss <lene.preuss@gmail.com>
2017-04-07 10:28:52 +0200

    added alias for /usr/bin/time for more info on a program

--- bash/.bash/alias.sh
+++ bash/.bash/alias.sh
@@ -4,90 +4,41 @@
 #   alias section
 #
 
-alias more=less
-alias lo=logout
-alias em='emacs -nw'
+# this is pretty neat and I'm ashamed I only learned this in 2017!
+alias time="/usr/bin/time --format='%E wall, %Us user, %Ss sys 
+%M kB max (%Xtext+%Ddata)
+%P CPU'"
 
-alias +r='chmod go+r'
-#alias '-r'='chmod go-r'
-alias +w='chmod go+w'
-#alias '-w'='chmod go-w'
-alias +x='chmod +x'
-#alias '-x'='chmod -x'
-
-# set options for some common commands
-alias	zip='zip -9mTu'
-alias	dir='ls -C'
-alias 	ls='ls $LS_OPTIONS'
-alias 	ll='ls -l'
-alias 	Xnest="xinit -- /usr/X11/bin/Xnest $1 -fn *-*-*-*-*-*-0-0-75-75-* \
-	     -fp '/usr/X11/lib/fonts/75dpi,/usr/X11/lib/fonts/misc'"
-alias 	xterm='xterm -bg rgb:e8/e8/e8 +ls -j -sb -sl 512'
-
-# i never used the following aliases, i will delete them in the next edit
-#alias h=history
-#alias del='rm -i'
-#alias rsz='eval `resize`'
-#alias mine='chown $USER'
-#alias vt52="set term = vt52"
-#alias vt100="set term = vt100"
-
-#alias   doc='less /usr/doc/'
-#alias   r='fc -s'
-
-# remove obsolete program from path (i'll keep this for sentimental reasons)
-alias ghostview=gv
-
-# From: pardo@cs.washington.edu (David Keppel)
-#alias   go='cd  $path_\!*'
-#alias   mark='set path_\!* = $cwd'
-#alias   marks='set | grep ^path_ | sed "s/^path_/     /"'
-#alias   unmark='unset path_\!*'
-
-################################################################################
-
-#   
-#   alias section
-#
-
-alias more=less
-alias lo=logout
-alias em='emacs -nw'
-
-alias +r='chmod go+r'
-#alias '-r'='chmod go-r'
-alias +w='chmod go+w'
-#alias '-w'='chmod go-w'
-alias +x='chmod +x'
-#alias '-x'='chmod -x'
-
-# set options for some common commands
-alias	zip='zip -9mTu'
-alias	dir='ls -C'
-alias 	ls='ls $LS_OPTIONS'
-alias 	ll='ls -l'
-alias 	Xnest="xinit -- /usr/X11/bin/Xnest $1 -fn *-*-*-*-*-*-0-0-75-75-* \
-	     -fp '/usr/X11/lib/fonts/75dpi,/usr/X11/lib/fonts/misc'"
-alias 	xterm='xterm -bg rgb:e8/e8/e8 +ls -j -sb -sl 512'
-
-# i never used the following aliases, i will delete them in the next edit
-#alias h=history
-#alias del='rm -i'
-#alias rsz='eval `resize`'
-#alias mine='chown $USER'
-#alias vt52="set term = vt52"
-#alias vt100="set term = vt100"
-
-#alias   doc='less /usr/doc/'
-#alias   r='fc -s'
-
-# remove obsolete program from path (i'll keep this for sentimental reasons)
-alias ghostview=gv
-
-# From: pardo@cs.washington.edu (David Keppel)
-#alias   go='cd  $path_\!*'
-#alias   mark='set path_\!* = $cwd'
-#alias   marks='set | grep ^path_ | sed "s/^path_/     /"'
-#alias   unmark='unset path_\!*'
-
+# haven't played WoW in years, but hey
 alias wow='wine .wine/drive_c/Program\ Files/World\ of\ Warcraft/Wow.exe -opengl'
+
+# all this is legit stuff from way back in the 90s and I did not even remember I had it
+
+alias more=less
+alias lo=logout
+alias em='emacs -nw'
+
+alias +r='chmod go+r'
+#alias '-r'='chmod go-r'
+alias +w='chmod go+w'
+#alias '-w'='chmod go-w'
+alias +x='chmod +x'
+#alias '-x'='chmod -x'
+
+# set options for some common commands
+# ^ commands that were common in the 90s, that is
+alias	zip='zip -9mTu'
+# who stil remembers the dir command? 
+alias	dir='ls -C'
+alias 	ls='ls $LS_OPTIONS'
+# yeah, this one I actually use all the time. only legit reason this file exists tbh.
+alias 	ll='ls -l'
+# lol, Xnest!
+alias 	Xnest="xinit -- /usr/X11/bin/Xnest $1 -fn *-*-*-*-*-*-0-0-75-75-* \
+	     -fp '/usr/X11/lib/fonts/75dpi,/usr/X11/lib/fonts/misc'"
+alias 	xterm='xterm -bg rgb:e8/e8/e8 +ls -j -sb -sl 512'
+
+# i'll keep this comment for sentimental reasons
+## remove obsolete program from path (i'll keep this for sentimental reasons)
+alias ghostview=gv
+
