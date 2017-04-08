5f4065f0ec8e1ddc62356929744a0fe494a95150
Lene Preuss <lene.preuss@gmail.com>
2017-04-08 18:02:16 +0200

    merges from work laptop

--- install.sh
+++ install.sh
@@ -26,4 +26,19 @@ if [ -f openbox/setlayout.c ]; then
 	cd ..
 fi
 
-
+# (try to) install xv
+cd /tmp
+wget ftp://ftp.trilon.com/pub/xv/xv-3.10a.tar.gz
+wget http://www.ulich.org/hints/resources/xv-3.10a-jumbo20050501-1.diff.gz
+wget http://www.ulich.org/hints/resources/xv-3.10a-jumbo-patches-20050501.tar.gz
+tar xvzf xv-3.10a.tar.gz
+tar xvzf xv-3.10a-jumbo-patches-20050501.tar.gz
+gzip -d xv-3.10a-jumbo20050501-1.diff.gz
+cd xv-3.10a
+patch -p1 < ../xv-3.10a-jumbo-fix-patch-20050410.txt
+patch -p1 < ../xv-3.10a-jumbo-enh-patch-20050501.txt
+patch -p1 < ../xv-3.10a-jumbo20050501-1.diff
+sudo apt install libxt-dev libc6-dev xlibs-dev libjpeg62-dev libtiff5-dev libpng12-dev libxt-dev
+make -j4
+sudo mv -i xv /usr/local/bin/
+cd -
