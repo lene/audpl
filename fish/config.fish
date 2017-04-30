82965e806e5e5884f7bc3294c47a7c01f26978f0
Lene Preuss <lene.preuss@gmail.com>
2017-04-30 17:40:14 +0200

    XPS on So 30. Apr 17:40:14 CEST 2017

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,8 @@
 eval (python -m virtualfish)
 eval (python -m virtualfish auto_activation global_requirements compat_aliases)
 
+function time -d "Use /usr/bin/time to display not only running time, but also memory usage"
+	/usr/bin/time --format='%E wall, %Us user, %Ss sys 
+%M kB max (%Xtext+%Ddata)
+%P CPU' $argv
+end
