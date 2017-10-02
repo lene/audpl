1e9dc5f486038c8c4b1b70bbacf8f933fa0fa6b0
Lene Preuss <lene.preuss@gmail.com>
2017-10-02 10:40:10 +0200

    XPS on Mo 2. Okt 10:40:10 CEST 2017

--- backup2.sh
+++ backup2.sh
@@ -1,5 +1,5 @@
 
-
+time sudo  rsync -av --progress --exclude /home --exclude /media/ --exclude /proc/ --exclude /dev/ --exclude /sys/ --exclude /run/ --exclude /tmp/ --exclude /var/tmp/ --exclude /mnt/ --delete-before / /media/lene/06ecd5c5-7380-476e-985b-211e32e5628b/
 sudo mkdir -p /media/lene/06ecd5c5-7380-476e-985b-211e32e5628b/home/lene/
 sudo chown lene.lene /media/lene/06ecd5c5-7380-476e-985b-211e32e5628b/home/lene/
 time rsync -av --progress --exclude .cache/ --exclude Azureus\ Downloads/ --exclude Downloads/Movies --exclude .local/share/Trash/ --exclude \*.bak --delete-before /home/lene/ /media/lene/06ecd5c5-7380-476e-985b-211e32e5628b/home/lene/
