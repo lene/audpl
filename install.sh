75f551d45aa7e5ccf8201b5e3a94d219891eb231
Lene Preuss <lene.preuss@gmail.com>
2017-10-25 18:40:06 +0200

    eris Mi 25. Okt 18:40:06 CEST 2017

--- install.sh
+++ install.sh
@@ -19,7 +19,7 @@ test -L $HOME/.mrxvtrc || ln -s $HOME/workspace/configs/.mrxvtrc $HOME
 test -L $HOME/.emacs   || ln -s $HOME/workspace/configs/.emacs   $HOME
 test -L $HOME/.xemacs  || ln -s $HOME/workspace/configs/.xemacs  $HOME
 
-sudo apt install mrxvt gkrelltop audacious zenity openbox emacs konsole xfce4 libx11-dev inotify-tools network-manager-gnome
+sudo apt install mrxvt gkrelltop audacious zenity openbox emacs konsole xfce4 libx11-dev inotify-tools network-manager-gnome tor rox-filer lxtask 
 
 if [ -f openbox/setlayout.c ]; then
     cd openbox && \
@@ -31,8 +31,24 @@ fi
 
 # (try to) install xv
 which xv || (
-    sudo apt install libxt-dev libc6-dev xlibs-dev libjpeg62-dev libtiff5-dev libpng12-dev && (
+	cd /tmp
+	sudo apt install libpng12-dev || (
+		test -f zlib-1.2.11.tar.gz || wget https://zlib.net/zlib-1.2.11.tar.gz
+		test -f libpng-1.2.54.tar.bz2 || wget https://sourceforge.net/projects/libpng/files/libpng12/older-releases/1.2.54/libpng-1.2.54.tar.bz2
+		tar xzf zlib-1.2.11.tar.gz
+		cd zlib-1.2.11
+		./configure --prefix=/usr/local
+		make -j4
+		sudo make install
 		cd /tmp
+		
+		tar xjf libpng-1.2.54.tar.bz2
+		cd libpng-1.2.54
+		./configure --prefix=/usr/local
+		make -j4
+		sudo make install
+	)
+    sudo apt install libxt-dev libc6-dev libjpeg62-dev libtiff5-dev  && (
 		test -f xv-3.10a.tar.gz || wget ftp://ftp.trilon.com/pub/xv/xv-3.10a.tar.gz
 		test -f xv-3.10a-jumbo20050501-1.diff.gz || wget http://www.ulich.org/hints/resources/xv-3.10a-jumbo20050501-1.diff.gz
 		test -f xv-3.10a-jumbo-patches-20050501.tar.gz || wget http://www.ulich.org/hints/resources/xv-3.10a-jumbo-patches-20050501.tar.gz
