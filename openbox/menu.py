8cd3d55adf2586cc679eaaa8bc66b07c3240ec28
Lene Preuss <lene.preuss@gmail.com>
2017-10-27 18:40:07 +0200

    eris Fr 27. Okt 18:40:07 CEST 2017

---
+++ openbox/menu.py
@@ -0,0 +1,15 @@
+#! /usr/bin/python3
+__author__ = 'Lene Preuss <lene.preuss@gmail.com>'
+
+print(
+"""
+<?xml version="1.0" encoding="UTF-8"?>
+<openbox_pipe_menu>
+  <item label="LABEL">
+    <action name="Execute">
+      <execute>xv</execute>
+    </action>
+  </item>
+</openbox_pipe_menu>
+"""
+)
