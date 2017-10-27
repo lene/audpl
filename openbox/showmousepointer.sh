9e93f3fbc3074f7819a0a7523cb6cdb0539030cd
Lene Preuss <lene.preuss@gmail.com>
2017-10-27 21:36:03 +0200

    key binding to pop up an image at the mouse pointer position

---
+++ openbox/showmousepointer.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/fish
+# pops up an image at the moise pointer position
+# needs xdotool anf pqiv installed, also assumes the image is present
+
+set IMG $HOME/workspace/configs/openbox/zoidberg.png
+set x (xdotool getmouselocation | cut -d ':' -f 2 | cut -d ' ' -f 1)
+set y (xdotool getmouselocation | cut -d ':' -f 3 | cut -d ' ' -f 1)
+# these values are specific to the image, to center the mouse pointer on the image
+set x (math $x-25)
+set y (math $y-35)
+pqiv -c -i -P $x,$y $IMG &
+sleep 3
+killall pqiv
