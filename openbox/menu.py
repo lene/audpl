9e0f89c892f9282d2b80117b97b9074b49ec877e
Lene Preuss <lene.preuss@gmail.com>
2017-10-27 19:33:20 +0200

    added an autogenerated menu which only contains executables which exist

--- openbox/menu.py
+++ openbox/menu.py
@@ -1,15 +1,58 @@
 #! /usr/bin/python3
+from collections import OrderedDict
+from shutil import which
+
 __author__ = 'Lene Preuss <lene.preuss@gmail.com>'
 
+programs = OrderedDict({
+    "Net": {
+        "Chromium": "x",
+        "Firefox": "x",
+        "Thunderbird": "x",
+        "Nicotine": "x",
+        "Skype": "x",
+        "Slack": "x",
+        "Pidgin": "x",
+        "Vuze": "x",
+        "VirtualBox": "x",
+        "KGPG": "x",
+    },
+    "Dev": {
+        "Emacs": "emacs",
+    },
+    "Fun": {
+        "Audacious": "audacious",
+    },
+    "KCalc": "kcalc",
+    "Kate": "kate",
+    "XV": "xv"
+})
+
+
+def create_menu(entries):
+    menu = ''
+    create_menu.indent += 4
+    for item in entries.keys():
+        indent = " " * create_menu.indent
+        if isinstance(entries[item], dict):
+            submenu = create_menu(entries[item])
+            if submenu:
+                menu += '{}<menu id="dynamic_{}" label="{}">\n{}{}</menu>\n'.format(
+                    indent, item.lower(), item, submenu, indent
+                )
+        else:
+            if which(entries[item]):
+                menu += '{}<item label="{}"><action name="execute"><execute>{}</execute></action></item>\n'.format(
+                    indent, item, entries[item]
+                )
+    create_menu.indent -= 4
+    return menu
+create_menu.indent = 0
+
 print(
-"""
-<?xml version="1.0" encoding="UTF-8"?>
+"""<?xml version="1.0" encoding="UTF-8"?>
 <openbox_pipe_menu>
-  <item label="LABEL">
-    <action name="Execute">
-      <execute>xv</execute>
-    </action>
-  </item>
+{}
 </openbox_pipe_menu>
-"""
+""".format(create_menu(programs))
 )
