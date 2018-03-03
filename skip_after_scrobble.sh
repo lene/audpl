c6229a2ce185eac6063987795d531016eadae2f1
Lene Preuss <lene.preuss@gmail.com>
2018-03-03 19:15:16 +0100

    ensure inotifywait is installed

--- skip_after_scrobble.sh
+++ skip_after_scrobble.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+which inotifywait > /dev/null || sudo apt install inotify-tools
+
 inotifywait -qq -e close_write $HOME/.config/audacious/scrobbler.log && \
   audtool current-song && \
   audtool playlist-delete $(audtool playlist-position) && \
