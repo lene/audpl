041349bd6db0ffdc6e25097d558d4fe75b1b1a80
Lene Preuss <lene.preuss@gmail.com>
2018-06-05 23:40:07 +0200

    eris Di 5. Jun 23:40:07 CEST 2018

--- backup3.sh
+++ backup3.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# sudo cryptsetup -y -v luksFormat --type luks2 /dev/sda1
+# password: same as laptop
+sudo cryptsetup open /dev/sda1 backup 
+# sudo mkfs.ext4 /dev/mapper/backup
+
+sudo mkdir -p /media/backup
+sudo mount /dev/mapper/backup /media/backup/
+
 TARGET_DRIVE_ID=06ecd5c5-7380-476e-985b-211e32e5628b/
 time sudo rsync -av --progress --exclude /media/ --exclude /proc/ --exclude /dev/ --exclude /sys/ --exclude /run/ --exclude .cache/ --exclude /tmp/ --exclude /var/tmp/ --exclude /mnt/ --exclude /home/ --exclude swapfile --delete-after / /media/lene/$TARGET_DRIVE_ID
 
