61780654976e3b0fc453af819e047e848ead4be1
Lene Preuss <lene.preuss@gmail.com>
2018-06-15 23:11:03 +0200

    better handling of errors and different backup devices

--- backup.sh
+++ backup.sh
@@ -1,23 +1,31 @@
 #!/bin/bash
 
 if [ $UID != 0 ]; then
-	echo "execute me with sudo plz thankyouverymuch"
+	echo "execute me with sudo plz tyvm"
 	exit
 fi
 
+DEVICE=${1:-/dev/sda1}
 TARGET_FOLDER=/media/backup
 
 # sudo cryptsetup -y -v luksFormat --type luks2 /dev/sda1
 # password: same as laptop
-cryptsetup open /dev/sda1 backup 
+cryptsetup open $DEVICE backup || exit 1
 # sudo mkfs.ext4 /dev/mapper/backup
 
 mkdir -p $TARGET_FOLDER
-mount /dev/mapper/backup $TARGET_FOLDER
+mount /dev/mapper/backup $TARGET_FOLDER || exit 1
 
-time rsync -av --progress --exclude /media/ --exclude /proc/ --exclude /dev/ --exclude /sys/ --exclude /run/ --exclude .cache/ --exclude /tmp/ --exclude /var/tmp/ --exclude /mnt/ --exclude /home/ --exclude swapfile --delete-after /  $TARGET_FOLDER
+time rsync -av --progress \
+	 --exclude /media/ --exclude /proc/ --exclude /dev/ --exclude /sys/ \
+	 --exclude /run/ --exclude .cache/ --exclude /tmp/ --exclude /var/tmp/ \
+	 --exclude /mnt/ --exclude /home/ --exclude swapfile \
+	 --delete-after /  $TARGET_FOLDER
 
-time rsync -av --progress --exclude .cache/ --exclude Azureus\ Downloads/ --exclude Downloads/Movies --exclude .local/share/Trash/ --exclude GoogleDrive --exclude \*.bak --delete-before /home/lene/ $TARGET_FOLDER/home/lene/
+time rsync -av --progress \
+	 --exclude .cache/ --exclude Azureus\ Downloads/ --exclude Downloads/Movies \
+	 --exclude .local/share/Trash/ --exclude GoogleDrive --exclude \*.bak \
+	 --delete-before /home/lene/ $TARGET_FOLDER/home/lene/
 
 umount /media/backup
 
