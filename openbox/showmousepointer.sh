dc46951c256d49c1885847e8da513cd020468a5b
Lene Preuss <lene.preuss@gmail.com>
2018-07-22 13:40:05 +0200

    eris So 22. Jul 13:40:05 CEST 2018

--- openbox/showmousepointer.sh
+++ openbox/showmousepointer.sh
@@ -3,11 +3,16 @@
 # needs xdotool anf pqiv installed, also assumes the image is present
 
 set IMG $HOME/workspace/configs/openbox/zoidberg.png
-set x (xdotool getmouselocation | cut -d ':' -f 2 | cut -d ' ' -f 1)
-set y (xdotool getmouselocation | cut -d ':' -f 3 | cut -d ' ' -f 1)
+set x 1000
+set y 500
+xdotool mousemove $x $y
+
+#set x (xdotool getmouselocation | cut -d ':' -f 2 | cut -d ' ' -f 1)
+#set y (xdotool getmouselocation | cut -d ':' -f 3 | cut -d ' ' -f 1)
 # these values are specific to the image, to center the mouse pointer on the image
 set x (math $x-25)
 set y (math $y-35)
+
 pqiv -c -i -P $x,$y $IMG &
 sleep 3
 killall pqiv
